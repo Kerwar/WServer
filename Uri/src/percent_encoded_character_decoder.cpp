c7ae18a6e1060f7919b9e1d0172726b62cc090c3
Javier <javi@DESKTOP-GC9VLLL>
2023-01-21 17:08:06 +0100

    Refactoring the percent encoded removing the state machine

--- Uri/src/percent_encoded_character_decoder.cpp
+++ Uri/src/percent_encoded_character_decoder.cpp
@@ -6,7 +6,24 @@ namespace Uri {
 struct PercentEncodedCharacterDecoder::Implementation
 {
   int decoded_character = 0;
-  Decode_state decode_state = first_digit_hex;
+  int digits_left = 2;
+
+  bool ShiftHexDigit(char character)
+  {
+    decoded_character *= HEX_DISPLACEMENT;
+    if (DIGITS.Contains(character)) {
+      decoded_character += character - '0';
+      return true;
+    } else if (CharacterSet('a', 'f').Contains(character)) {
+      decoded_character += character - 'a' + LETTER_DISPLACEMENT;
+      return true;
+    } else if (CharacterSet('A', 'F').Contains(character)) {
+      decoded_character += character - 'A' + LETTER_DISPLACEMENT;
+      return true;
+    }
+
+    return false;
+  }
 };
 
 PercentEncodedCharacterDecoder::PercentEncodedCharacterDecoder()
@@ -23,52 +40,22 @@ PercentEncodedCharacterDecoder &PercentEncodedCharacterDecoder::operator=(
 
 bool PercentEncodedCharacterDecoder::NextEncodedCharacter(char character)
 {
-  switch (impl_->decode_state) {
-
-  case first_digit_hex:
-      impl_->decode_state = second_digit_hex;
-    if (DIGITS.Contains(character)) {
-      impl_->decoded_character = character - '0';
-      break;
-    } else if (CharacterSet('a', 'f').Contains(character)) {
-      impl_->decoded_character += character - 'a' + LETTER_DISPLACEMENT;
-      break;
-    } else if (CharacterSet('A', 'F').Contains(character)) {
-      impl_->decoded_character += character - 'A' + LETTER_DISPLACEMENT;
-      break;
-    }
-    return false;
-
-  case second_digit_hex:
-    impl_->decode_state = done;
-    impl_->decoded_character *= HEX_DISPLACEMENT;
-    if (DIGITS.Contains(character)) {
-      impl_->decoded_character += character - '0';
-      break;
-    } else if (CharacterSet('a', 'f').Contains(character)) {
-      impl_->decoded_character += character - 'a' + LETTER_DISPLACEMENT;
-      break;
-    } else if (CharacterSet('A', 'F').Contains(character)) {
-      impl_->decoded_character += character - 'A' + LETTER_DISPLACEMENT;
-      break;
-    }
-    return false;
-
-  case done:
-    return false;
+  if (impl_->digits_left > 0) {
+    --impl_->digits_left;
+    return impl_->ShiftHexDigit(character);
   }
-
-  return true;
+  return false;
 }
 
 bool PercentEncodedCharacterDecoder::Done() const
 {
-  return impl_->decode_state == done;
+  return impl_->digits_left == 0;
 }
 
 char PercentEncodedCharacterDecoder::GetDecodedCharacter() const
 {
   return static_cast<char>(impl_->decoded_character);
 }
+
 
 }// namespace Uri
